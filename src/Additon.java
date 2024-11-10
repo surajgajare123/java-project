511a9001941eb4c0f9494ee15461214ead988477
suraj <surajgajare021@gmail.com>
2024-11-10 09:16:52 +0530

    my first java program

---
+++ src/Additon.java
@@ -0,0 +1,10 @@
+
+public class Additon {
+    public static void main(String[] args) {
+        System.out.println("Addition of two numbers : ");
+        int a = 12;
+        int b = 45;
+        int c = a +b;
+        System.out.println(c);
+    }
+}
